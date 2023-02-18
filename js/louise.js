536014bb48147f2ebca39af7f6b34946401b096c
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-02-19 00:33:39 +0900

    First version

---
+++ js/louise.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function generateLouiseCopypasta(louise) {
+	return `${louise}！${louise}！${louise}！${louise}ぅぅうううわぁああああああああああああああああああああああん！！！
+あぁああああ…ああ…あっあっー！あぁああああああ！！！${louise}${louise}${louise}ぅううぁわぁああああ！！！
+あぁクンカクンカ！クンカクンカ！スーハースーハー！スーハースーハー！いい匂いだなぁ…くんくん
+んはぁっ！${louise}・フランソワーズたんの桃色ブロンドの髪をクンカクンカしたいお！クンカクンカ！あぁあ！！
+間違えた！モフモフしたいお！モフモフ！モフモフ！髪髪モフモフ！カリカリモフモフ…きゅんきゅんきゅい！！
+小説12巻の${louise}たんかわいかったよぅ！！あぁぁああ…あああ…あっあぁああああ！！ふぁぁあああんんっ！！
+アニメ2期放送されて良かったね${louise}たん！あぁあああああ！かわいい！${louise}たん！かわいい！あっああぁああ！
+コミック2巻も発売されて嬉し…いやぁああああああ！！！にゃああああああああん！！ぎゃああああああああ！！
+ぐあああああああああああ！！！コミックなんて現実じゃない！！！！あ…小説もアニメもよく考えたら…
+${[...louise].join(' ')} ち ゃ ん は 現実 じ ゃ な い？にゃあああああああああああああん！！うぁああああああああああ！！
+そんなぁああああああ！！いやぁぁぁあああああああああ！！はぁああああああん！！ハルケギニアぁああああ！！
+この！ちきしょー！やめてやる！！現実なんかやめ…て…え！？見…てる？表紙絵の${louise}ちゃんが僕を見てる？
+表紙絵の${louise}ちゃんが僕を見てるぞ！${louise}ちゃんが僕を見てるぞ！挿絵の${louise}ちゃんが僕を見てるぞ！！
+アニメの${louise}ちゃんが僕に話しかけてるぞ！！！よかった…世の中まだまだ捨てたモンじゃないんだねっ！
+いやっほぉおおおおおおお！！！僕には${louise}ちゃんがいる！！やったよケティ！！ひとりでできるもん！！！
+あ、コミックの${louise}ちゃああああああああああああああん！！いやぁあああああああああああああああ！！！！
+あっあんああっああんあアン様ぁあ！！シ、シエスター！！アンリエッタぁああああああ！！！タバサｧぁあああ！！
+ううっうぅうう！！俺の想いよ${louise}へ届け！！ハルゲニアの${louise}へ届け！`
+}
+
+function handleSubmit(_) {
+	copypasta.value = generateLouiseCopypasta(louise_name.value);
+}
+
+louise.addEventListener('submit', e => {
+	e.preventDefault();
+	handleSubmit(e);
+	return false;
+});
+
+copy.addEventListener('click', _ => {
+	copypasta.select();
+	document.execCommand('copy');
+});
+
+twitter.addEventListener('click', _ => {
+	const louiseUrl = 'https://kusaremkn.github.io/louise/';
+	const tweetUrl = new URL('https://twitter.com/intent/tweet/');
+	tweetUrl.search += 'text=' + encodeURIComponent(copypasta.value);
+	tweetUrl.search += '&url=' + encodeURIComponent(louiseUrl);
+	window.open(tweetUrl.toString());
+});
